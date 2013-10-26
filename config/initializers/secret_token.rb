343b9bf9543fd637c6d6d44588888c1b8b37fd39
goldtopgirl <goldtop.mail+code@gmail.com>
2013-10-26 14:24:16 +0100

    New app install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweeter::Application.config.secret_key_base = '9476195d36bed24b366b81296ab49bf04ba2009a1ed373c81c71a9e8f214be01ce7d852837e5724ccda52874749ec9d7406824bf062cd16b2c211527e2163720'
